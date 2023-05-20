5f2eb26e50099ef2731e410855608da08c77801c
saajanrana <saajanrana001@gmail.com>
2023-05-20 20:48:51 +0530

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const search = ()=>{
     const searchbox= document.getElementById('search-item').value.toUpperCase();
     const storeitems= document.getElementById('product-list');
-    const product = document.querySelectorAll(".product".);
+    const product = document.querySelectorAll(".product");
     const pname = document.getElementsByTagName("h2");
 
 
